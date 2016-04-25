daa182283939c2f8ff9f3f4d583dd48542be5534
paul <papattyn@gmail.com>
2016-04-25 14:58:53 +0200

    Added : Notification bar (click it and the app stops)

--- app/src/main/java/fr/urbotteam/urbot/UrbotActivity.java
+++ app/src/main/java/fr/urbotteam/urbot/UrbotActivity.java
@@ -1,23 +1,27 @@
 package fr.urbotteam.urbot;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import fr.urbotteam.urbot.Bluetooth.UrbotBluetoothService;
 
 public class UrbotActivity extends Activity {
-    // TODO : - Notification bar
-
     private static final String TAG = "CameraDebug";
     private CameraService mCameraService;
     private UrbotBluetoothService urbotBluetoothService;
     private boolean mBound;
+    private final int mId = 42;
 
 
     @Override
@@ -25,12 +29,50 @@ public class UrbotActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_camera);
 
-        Intent intent = new Intent(this, UrbotBluetoothService.class);
-        bindService(intent, mBluetoothConnection, Context.BIND_AUTO_CREATE);
+        if(savedInstanceState == null) {
+            Intent intent = new Intent(this, UrbotBluetoothService.class);
+            bindService(intent, mBluetoothConnection, Context.BIND_AUTO_CREATE);
 
-        intent = new Intent(this, CameraService.class);
-        bindService(intent, mCameraConnection, Context.BIND_AUTO_CREATE);
+            intent = new Intent(this, CameraService.class);
+            bindService(intent, mCameraConnection, Context.BIND_AUTO_CREATE);
+
+            initNotification();
+        } else {
+            Log.d(TAG, "onCreate savedInstanceState non null");
+        }
     }
+
+    // Create Notification
+    private void initNotification() {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Urbot")
+                        .setContentText("Running");
+
+        mBuilder.setOngoing(true);
+        mBuilder.setAutoCancel(true);
+
+        //Register a receiver to stop Service
+        registerReceiver(stopServiceReceiver, new IntentFilter("myFilter"));
+        PendingIntent contentIntent = PendingIntent.getBroadcast(this, 0, new Intent("myFilter"), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(contentIntent);
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(mId, mBuilder.build());
+    }
+
+
+    //We need to declare the receiver with onReceive function as below
+    protected BroadcastReceiver stopServiceReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            finish();
+        }
+    };
 
     /**
      * Releases the resources associated with the camera source, the associated detector, and the
